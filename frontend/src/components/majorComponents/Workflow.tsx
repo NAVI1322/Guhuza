59f5cfb6b52636f5326a0488be0ecda2b835a68a
visioncrunch <ganubhav162@gmail.com>
2024-10-10 17:51:38 +0530

    dddd

--- frontend/src/components/majorComponents/Workflow.tsx
+++ frontend/src/components/majorComponents/Workflow.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle2 } from "lucide-react";
 import codeImg from "../assets/code.jpg";
-import { checklistItems } from "../constants";
+import { checklistItems } from "@/constants";
+
 
 const Workflow = () => {
   return (
@@ -16,7 +17,7 @@ const Workflow = () => {
           <img src={codeImg} alt="Coding" />
         </div>
         <div className="pt-12 w-full lg:w-1/2">
-          {checklistItems.map((item, index) => (
+          {checklistItems.map((item:any, index:any) => (
             <div key={index} className="flex mb-12">
               <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                 <CheckCircle2 />
